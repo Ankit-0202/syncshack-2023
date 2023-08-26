ef48885914a92d899f7198eda3c00cdc470d05e7
cpolish <chrisp2332@icloud.com>
2023-08-26 20:15:56 +1000

    Ankit made changes

--- src/components/PromptForm.jsx
+++ src/components/PromptForm.jsx
@@ -1,52 +1,70 @@
 import { useState } from 'react'
 import { Button, Grid, TextField } from '@mui/material';
 
-function submitPrompt(event, prompt, setPromptCallback) {
-  event.preventDefault()
-
-  const jsonData = {prompt}
-  fetch("http://localhost:5000/prompt-processing", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(jsonData),
-  }).then((data) => {
-    return data.json()
-  }).then((jsonResponseData) => {
-    if (jsonResponseData.status != "OK") {
-      throw new Error("bad status")
-    }
-  }).catch((error) => {
-    console.error(error)
-  })
-
-  setPromptCallback("")
-}
-
 export default function PromptForm() {
   const [prompt, setPrompt] = useState("")
+  const [mood, setMood] = useState("")
+
+  function clearForm() {
+    setPrompt("")
+    setMood("")
+  }
+
+  function submitPrompt(event) {
+    event.preventDefault()
+  
+    const jsonData = {prompt, mood}
+    fetch("http://localhost:5000/prompt-processing", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(jsonData),
+    }).then((data) => {
+      return data.json()
+    }).then((jsonResponseData) => {
+      if (jsonResponseData.status != "OK") {
+        throw new Error("bad status")
+      }
+    }).catch((error) => {
+      console.error(error)
+    })
+  
+    clearForm()
+  }
 
   return (
     <Grid
       component="form"
       container
       spacing={2}
-      onSubmit={(e) => submitPrompt(e, prompt, setPrompt)}
+      onSubmit={submitPrompt}
     >
-        <Grid item xs={12}>
-          <TextField
-            id="prompt-input"
-            label="Prompt input"
-            multiline
-            fullWidth
-            value={prompt}
-            onChange={(e) => setPrompt(e.target.value)}
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <Button type="submit" variant="contained">Send Prompt</Button>
-        </Grid>
+      <Grid item xs={12}>
+        <TextField
+          id="prompt-input"
+          label="Prompt input"
+          multiline
+          fullWidth
+          value={prompt}
+          onChange={(e) => setPrompt(e.target.value)}
+        />
+      </Grid>
+
+      <Grid item xs={12}>
+        <TextField
+          id="mood-input"
+          label="Mood"
+          helperText="Enter a mood to jazz up your presentation!"
+          fullWidth
+          value={mood}
+          onChange={(e) => setMood(e.target.value)}
+        />
+      </Grid>
+
+      <Grid item xs={12}>
+        <Button type="submit" variant="contained">Send Prompt</Button>
+      </Grid>
     </Grid>
   )
 }
