bb5a9a2f874aef65aa325678411606f735447385
Tim <yyan0195@uni.sydney.edu.au>
2023-08-26 16:12:25 +1000

    added image generation api calls

--- my_openai.py
+++ my_openai.py
@@ -1,7 +1,13 @@
 import os
 import openai
+import json
+import requests 
 
-openai.api_key = "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"
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+SD_API_KEY = os.getenv("SD_API_KEY")
+
+openai.api_key = os.getenv(OPENAI_API_KEY)
+print(openai.api_key);
 
 def openai_prompt(my_prompt: str):
     response = openai.Completion.create(
@@ -11,8 +17,46 @@ def openai_prompt(my_prompt: str):
     )
     return response.choices[0].text.strip()
 
+def generate_image(my_prompt: str, neg_prompt:str):
+    url = "https://stablediffusionapi.com/api/v3/text2img"
+
+    payload = json.dumps({
+        "key": SD_API_KEY,
+        "prompt": my_prompt,
+        "negative_prompt": None if len(neg_prompt) == 0,
+        "width": "512",
+        "height": "512",
+        "samples": "1",
+        "num_inference_steps": "20",
+        "seed": None,
+        "guidance_scale": 7.5,
+        "safety_checker": "yes",
+        "multi_lingual": "no",
+        "panorama": "no",
+        "self_attention": "no",
+        "upscale": "no",
+        "webhook": None,
+        "track_id": None
+    })
+
+    headers = {
+    'Content-Type': 'application/json'
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+    print("Please find your image at", response.json())
 
 
 if __name__ == "__main__":
-    prompt = input()
-    print(openai_prompt(prompt))
+    option = input("Choose an option (A) generate text (B) generate image: ")
+    option = option.upper()
+    if option != "A" and option != "B":
+        print("Invalid option");
+        exit();
+    
+    prompt = input("Please enter a prompt: ")
+    if (option == "B"):
+        neg_prompt = input("Please enter a negative prompt (leave empty for nothing): ")
+        generate_image(prompt, neg_prompt)
+    elif (option == "A"):
+        print(openai_prompt(prompt))
--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>YYOY</p>
+        <p>YYOYsadfasdf</p>
         <p>
           <button type="button" onClick={() => setCount((count) => count + 1)}>
             count is: {count}
