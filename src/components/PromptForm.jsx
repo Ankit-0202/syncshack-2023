c3615a285460f0474531a2970d6fcbdced849748
cpolish <chrisp2332@icloud.com>
2023-08-26 19:27:36 +1000

    add communication between frontend and backend

--- src/components/PromptForm.jsx
+++ src/components/PromptForm.jsx
@@ -1,11 +1,39 @@
 import { useState } from 'react'
-import { Box, Button, Grid, TextField } from '@mui/material';
+import { Button, Grid, TextField } from '@mui/material';
+
+function submitPrompt(event, prompt, setPromptCallback) {
+  event.preventDefault()
+
+  const jsonData = {prompt}
+  fetch("http://localhost:5000/prompt-processing", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(jsonData),
+  }).then((data) => {
+    return data.json()
+  }).then((jsonResponseData) => {
+    if (jsonResponseData.status != "OK") {
+      throw new Error("bad status")
+    }
+  }).catch((error) => {
+    console.error(error)
+  })
+
+  setPromptCallback("")
+}
 
 export default function PromptForm() {
   const [prompt, setPrompt] = useState("")
 
   return (
-    <Grid component="form" container spacing={2}>
+    <Grid
+      component="form"
+      container
+      spacing={2}
+      onSubmit={(e) => submitPrompt(e, prompt, setPrompt)}
+    >
         <Grid item xs={12}>
           <TextField
             id="prompt-input"
