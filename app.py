fe9818735ce932e2cc16d7f7b7978826dfa09e53
akap2114 <akap2114@uni.sydney.edu.au>
2023-08-26 20:11:51 +1000

    Ankit made changes

--- app.py
+++ app.py
@@ -7,15 +7,11 @@ app = Flask(__name__)
 
 @app.post("/prompt-processing")
 def process_prompt():
-    # TODO: Backend prompt processing
 
     # Get JSON data sent with POST request
     json_data = request.json
     # Access prompt data (under "prompt" key)
     prompt_text = json_data["prompt"] # type: ignore
-    
-    print(prompt_text)
-    
     
     out = ""
     
@@ -26,11 +22,8 @@ def process_prompt():
     else:
         out = generate_text(prompt_text)
     
-    
+    print(prompt_text)
     print(out)
-    
-    
-    
 
     response = {
         "status": "OK",
