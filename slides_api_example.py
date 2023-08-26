a37e355b97f68e0c6d265c4c352b813342eae805
Tim <yyan0195@uni.sydney.edu.au>
2023-08-26 18:22:06 +1000

    added slides api examples

--- slides_api_example.py
+++ slides_api_example.py
@@ -6,17 +6,16 @@ from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
-
+import google
 # If modifying these scopes, delete the file token.json.
-SCOPES = ['https://www.googleapis.com/auth/presentations.readonly']
+# List of permissions that google asks you to authenticate for 
+SCOPES = ['https://www.googleapis.com/auth/presentations.readonly', "https://www.googleapis.com/auth/presentations"]
 
 # The ID of a sample presentation.
-PRESENTATION_ID = '1EAYk18WDjIG-zp_0vLm3CsfQh_i8eXc67Jo2O9C6Vuc'
+PRESENTATION_ID = '1IlA5ES-gKdA_ySNXK3SsiQD3D0Oo8NhCSqby7VGrqPQ'
 
-def main():
-    """Shows basic usage of the Slides API.
-    Prints the number of slides and elements in a sample presentation.
-    """
+
+def get_credentials():
     creds = None
     # The file token.json stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
@@ -34,6 +33,59 @@ def main():
         # Save the credentials for the next run
         with open('token.json', 'w') as token:
             token.write(creds.to_json())
+    return creds
+
+
+def create_slide(presentation_id, page_id, creds):
+    """
+    Creates the Presentation the user has access to.
+    Load pre-authorized user credentials from the environment.
+    TODO(developer) - See https://developers.google.com/identity
+    for guides on implementing OAuth2 for the application.\n
+    """
+    # pylint: disable=maybe-no-member
+    try:
+        service = build('slides', 'v1', credentials=creds)
+        # Add a slide at index 1 using the predefined
+        # 'TITLE_AND_TWO_COLUMNS' layout and the ID page_id.
+        requests = [
+            {
+                'createSlide': {
+                    'objectId': page_id,
+                    'insertionIndex': '1',
+                    'slideLayoutReference': {
+                        'predefinedLayout': 'TITLE_AND_TWO_COLUMNS'
+                    }
+                }
+            }
+        ]
+
+        # If you wish to populate the slide with elements,
+        # add element create requests here, using the page_id.
+
+        # Execute the request.
+        body = {
+            'requests': requests
+        }
+        response = service.presentations() \
+            .batchUpdate(presentationId=presentation_id, body=body).execute()
+        create_slide_response = response.get('replies')[0].get('createSlide')
+        print(f"Created slide with ID:"
+              f"{(create_slide_response.get('objectId'))}")
+    except HttpError as error:
+        print(f"An error occurred: {error}")
+        print("Slides not created")
+        return error
+
+    return response
+
+def add_image_to_slide(image, creds):
+    pass
+
+def list_slides(creds):
+    """Shows basic usage of the Slides API.
+    Prints the number of slides and elements in a sample presentation.
+    """
 
     try:
         service = build('slides', 'v1', credentials=creds)
@@ -41,6 +93,7 @@ def main():
         # Call the Slides API
         presentation = service.presentations().get(
             presentationId=PRESENTATION_ID).execute()
+       
         slides = presentation.get('slides')
 
         print('The presentation contains {} slides:'.format(len(slides)))
@@ -49,5 +102,12 @@ def main():
                 i + 1, len(slide.get('pageElements'))))
     except HttpError as err:
         print(err)
+
+
+def main():
+    creds = get_credentials();
+    #NB: Object id for slide must have length >= 5
+    create_slide(PRESENTATION_ID, "sample", creds);
+
 
 main()
