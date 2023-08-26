424b1186f1ce18eea159dc6d36f32d275be8be7c
Daniel Geng <gengruize@gmail.com>
2023-08-26 18:01:54 +1000

    simple mood modification langchain

---
+++ mood_langchain.py
@@ -0,0 +1,41 @@
+#pip install langchain openai
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts.chat import (
+    ChatPromptTemplate,
+    SystemMessagePromptTemplate,
+    HumanMessagePromptTemplate,
+)
+from langchain.chains import LLMChain
+from langchain.schema import BaseOutputParser
+
+class RawOutputParser(BaseOutputParser):
+    def parse(self, text:str):
+        return text
+
+chat = ChatOpenAI(openai_api_key="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")
+
+def langchainApply(template: str, humanPrompt: str):
+    
+    # template = """You are an assistant who generates words depending on the user's adjective and mood.
+    # A user will pass in two words. The first word will contain the adjective and the second word will contain the mood;
+    # you should generate 5 synonyms of that adjective that fits the mood in a comma separated list.
+    # ONLY return a comma separated list, and nothing more."""
+
+    system_message_prompt = SystemMessagePromptTemplate.from_template(template)
+    human_template = "{text}"
+    human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
+
+    chat_prompt = ChatPromptTemplate.from_messages([system_message_prompt, human_message_prompt])
+    chain = LLMChain(
+        llm=chat,
+        prompt=chat_prompt,
+        output_parser=RawOutputParser()
+    )
+    return chain.run(humanPrompt)
+
+if __name__ == "__main__":
+    template = """You are an assistant who generates words depending on the user's adjective and mood.
+    A user will pass in two words. The first word will contain the adjective and the second word will contain the mood;
+    you should generate 5 synonyms of that adjective that fits the mood in a comma separated list.
+    ONLY return a comma separated list, and nothing more."""
+    print(langchainApply(template, "love angry"))
--- my_ai.py
+++ my_ai.py
@@ -4,20 +4,21 @@ import json
 import requests 
 import os.path
 
+import mood_langchain
 
 #OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 SD_API_KEY = os.getenv("SD_API_KEY")
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
-print(openai.api_key)
 
 def openai_prompt(my_prompt: str):
-    response = openai.Completion.create(
-        engine="text-davinci-003",
-        prompt=my_prompt,
-        max_tokens=10
-    )
-    return response.choices[0].text.strip()
+
+    template = """You are an assistant who generates words depending on the user's adjective and mood.
+    A user will pass in two words. The first word will contain the adjective and the second word will contain the mood;
+    you should generate 5 synonyms of that adjective that fits the mood in a comma separated list.
+    ONLY return a comma separated list, and nothing more."""
+    print("input two words, adjective and mood modifier")
+    print(mood_langchain.langchainApply(template, my_prompt))
 
 def generate_image(my_prompt: str, neg_prompt:str):
     url = "https://stablediffusionapi.com/api/v3/text2img"
