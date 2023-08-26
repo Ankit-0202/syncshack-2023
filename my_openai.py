dffc7f71168ca9c7a14f9d9b62e82a8881950f03
akap2114 <akap2114@uni.sydney.edu.au>
2023-08-26 15:07:37 +1000

    Ankit made changes

--- my_openai.py
+++ my_openai.py
@@ -1,6 +1,19 @@
 import os
 import openai
 
-openai.organization = "YOUR_ORG_ID"
-openai.api_key = os.getenv("OPENAI_API_KEY")
-openai.Model.list()
+openai.api_key = "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"
+
+def openai_prompt(my_prompt: str):
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=my_prompt,
+        max_tokens=10
+    )
+    
+    return response.choices[0].text.strip()
+
+
+
+if __name__ == "__main__":
+    prompt = input()
+    print(openai_prompt(prompt))
