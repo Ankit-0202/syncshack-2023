7c3e83a76ba0db9571e962032ed33cae9d47b469
cpolish <chrisp2332@icloud.com>
2023-08-26 17:44:16 +1000

    more Flask setup

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 
 app = Flask(__name__)
@@ -7,6 +7,12 @@ app = Flask(__name__)
 @app.post("/prompt-processing")
 def process_prompt():
     # TODO: Backend prompt processing
+
+    # Get JSON data sent with POST request
+    json_data = request.json
+    # Access prompt data (under "prompt" key)
+    prompt_text = json_data["prompt"] # type: ignore
+
     response = {
         "status": "OK"
     }
