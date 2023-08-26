c01d67b28c3579827febe844d9430a54a935b102
akap2114 <akap2114@uni.sydney.edu.au>
2023-08-26 18:20:48 +1000

    Ankit made changes

--- my_ai.py
+++ my_ai.py
@@ -12,22 +12,13 @@ SD_API_KEY = os.getenv("SD_API_KEY")
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
-def openai_prompt(my_prompt: str):
-
+def generate_text(my_prompt: str):
     template = """You are an assistant who generates words depending on the user's adjective and mood.
     A user will pass in two words. The first word will contain the adjective and the second word will contain the mood;
     you should generate 5 synonyms of that adjective that fits the mood in a comma separated list.
     ONLY return a comma separated list, and nothing more."""
-    print("input two words, adjective and mood modifier")
-    print(mood_langchain.langchainApply(template, my_prompt))
-
-def generate_text(my_prompt: str):
-    response = openai.Completion.create(
-        engine="text-davinci-003",
-        prompt=my_prompt,
-        max_tokens=10
-    )
-    return response.choices[0].text.strip()
+    # print("Input two words, adjective and mood modifier")
+    return mood_langchain.langchainApply(template, my_prompt)
 
 
 def generate_image(my_prompt: str, neg_prompt: str):
@@ -53,7 +44,7 @@ def generate_image(my_prompt: str, neg_prompt: str):
     })
 
     headers = {
-    'Content-Type': 'application/json'
+        'Content-Type': 'application/json'
     }
 
     response = requests.request("POST", url, headers=headers, data=payload)
@@ -70,7 +61,7 @@ if __name__ == "__main__":
     prompt = input("Please enter a prompt: ")
     
     if (option == "A"):
-        print(openai_prompt(prompt))
+        print(generate_text(prompt))
     elif (option == "B"):
         neg_prompt = input("Please enter a negative prompt (leave empty for nothing): ")
         print(generate_image(prompt, neg_prompt))
